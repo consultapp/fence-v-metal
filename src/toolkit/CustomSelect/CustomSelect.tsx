3ee0ace2b9406533670d094e6716e1ed3ce8204a
Dmitry Egorov <info@consultapp.ru>
2024-01-31 17:57:36 +0300

    bug fixes select

--- src/toolkit/CustomSelect/CustomSelect.tsx
+++ src/toolkit/CustomSelect/CustomSelect.tsx
@@ -19,22 +19,39 @@ export default function CustomSelect({
   const current = useAppSelector(selector);
   const dispatch = useAppDispatch();
   const [isOpen, setIsOpen] = useState(false);
-
-  const changeHandler = (e: React.MouseEvent<HTMLDivElement>) => {
-    dispatch(dispatcher(Number(e.currentTarget.dataset.id)));
-    setIsOpen(false);
-  };
-
   const [currentProduct] = useMemo(() => {
     return products.filter((item) => item.id === current);
   }, [products, current]);
 
+  const changeHandler = (e: React.MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+    dispatch(dispatcher(Number(e.currentTarget.dataset.id)));
+    setIsOpen(false);
+  };
+
+  const toggleSelect = (e: React.MouseEvent<HTMLDivElement>) => {
+    // e.stopPropagation();
+    if (isOpen) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+      const controller = new AbortController();
+      document.addEventListener(
+        "click",
+        (event) => {
+          if (event.target !== e.target) {
+            setIsOpen(false);
+            controller.abort();
+          }
+        },
+        { signal: controller.signal }
+      );
+    }
+  };
+
   return (
     <div className="fenceSelect" data-selected>
-      <div
-        className="fenceSelect__header"
-        onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
-      >
+      <div className="fenceSelect__header" onClick={toggleSelect}>
         <div className="fenceSelect__current" data-empty>
           {currentProduct ? (
             <>
@@ -61,7 +78,9 @@ export default function CustomSelect({
       {isOpen && (
         <div className="fenceSelect__body">
           <div className="fenceSelect__option">
-            <div className="fenceSelect__noinfo">Не выбрано</div>
+            <div className="fenceSelect__noinfo" key="option_noinfo">
+              Не выбрано
+            </div>
           </div>
           {products.map((product) => (
             <div
@@ -70,6 +89,7 @@ export default function CustomSelect({
               data-selectable
               data-selected={product.id === current}
               onClick={changeHandler}
+              key={product.id}
             >
               <div className="fenceSelect__info">
                 <div>{product.name}</div>
